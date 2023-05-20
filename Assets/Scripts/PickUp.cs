0e719c1e46c1f888cf79f5160e7c0b8272b0df26
Edvaldo Batista <94077173+Azulrok@users.noreply.github.com>
2023-05-20 11:01:37 -0300

    Finalizando pick up

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -17,15 +17,20 @@ public class PickUp : MonoBehaviour
         {
             if(itemHolding)
             {
-
+                itemHolding.transform.position = transform.position + Direction;
+                itemHolding.transform.parent = null;
+                if (itemHolding.GetComponent<Rigidbody2D>())
+                        itemHolding.GetComponent<Rigidbody2D>().simulated = true;
+                itemHolding = null;
             }
             else
             {   //position + direcao do personagem magnitude = 1, radious, mask
                 Collider2D pickUpItem = Physics2D.OverlapCircle(transform.position + Direction, .4f, pickUpMask);
-                if(pickUpItem)
+                if (pickUpItem)
                 {
                     itemHolding = pickUpItem.gameObject;
                     itemHolding.transform.position = holdSpot.position;
+                    itemHolding.transform.parent = transform;
                     if (itemHolding.GetComponent<Rigidbody2D>())
                         itemHolding.GetComponent<Rigidbody2D>().simulated = false;
                 }
