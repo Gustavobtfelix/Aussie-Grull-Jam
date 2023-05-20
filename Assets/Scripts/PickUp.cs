506686820d16cf95a68d7fcd2ecab36309f3c2bf
Edvaldo Batista <94077173+Azulrok@users.noreply.github.com>
2023-05-20 10:54:40 -0300

    Configurando pickup e box collider

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -6,17 +6,30 @@ public class PickUp : MonoBehaviour
 {
     public Transform holdSpot;
     public LayerMask pickUpMask;
-
-    // public Vector3 Direction { get; set; };
-
-    void Start()
-    {
-        
-    }
+    public GameObject destroyEffect;
+    public Vector3 Direction { get; set; }
+    private GameObject itemHolding;
 
     
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(itemHolding)
+            {
+
+            }
+            else
+            {   //position + direcao do personagem magnitude = 1, radious, mask
+                Collider2D pickUpItem = Physics2D.OverlapCircle(transform.position + Direction, .4f, pickUpMask);
+                if(pickUpItem)
+                {
+                    itemHolding = pickUpItem.gameObject;
+                    itemHolding.transform.position = holdSpot.position;
+                    if (itemHolding.GetComponent<Rigidbody2D>())
+                        itemHolding.GetComponent<Rigidbody2D>().simulated = false;
+                }
+            }
+        }
     }
 }
